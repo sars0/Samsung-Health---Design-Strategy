9a0f86de2f512ef7b6cc3148d882741da89b98ee
Sejeong Seo <16846477-SEOSEJ@users.noreply.replit.com>
2024-09-10 16:00:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,122 @@
+
+
+
+
+
+
+
+
+
+// I put the Javascript code in **wheel.html** file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // define a function to make 'Wheeling' button clickable
+// function wheeling()
+// //   perform tasks when the button is pressed
+// //     or instead of putting wheeling button already on the 'workout' section, i may put it on the 'edit' section
+
+// // make a button to customize the features on the 'home' menu
+// //   or i could also keep the original design of samsung health, which is to 'press and hold' for a few seconds for the customizing page to show up
+
+// // also try adding the animations for the workout data graphs
+// // define workout data
+// workoutData = [];
+// // create a graph object using chart.js library ** need source code here
+// data = graphW("workoutGraph", workoutData);
+// // define animation
+// var animation = {
+//   duration: 1500,
+//   // show graph going up
+//   from: { opacity: 0 },
+//   to: { opacity: 1 },
+//   easing: 'easeInQuart',
+//   complete: function() {
+//   }
+// };
